814f3be558b92a208a8d294ea8948a7071824122
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-11-09 19:07:08 +0100

    a

---
+++ U3/src/ejercicio03/Main.java
@@ -0,0 +1,38 @@
+package ejercicio03;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.print("Introduce el número de hilos: ");
+        int numeroHilos = new Scanner(System.in).nextInt();
+        ArrayList<Thread> listaHilos = new ArrayList<>();
+        for(int i = 0; i < numeroHilos; i++)
+        {
+            Thread hilo = new Thread(new Hilo(i));
+            listaHilos.add(hilo);
+            hilo.start();
+        }
+        int hiloTerminado = 0;
+        Thread thread;
+        while(hiloTerminado < listaHilos.size())
+        {
+            hiloTerminado = 0;
+            for(int i = 0; i < listaHilos.size(); i++)
+            {
+                thread = listaHilos.get(i);
+                System.out.println("Hilo " + i + ": " + listaHilos.get(i).getState());
+                if(listaHilos.get(i).getState() == Thread.State.TERMINATED)
+                {
+                    hiloTerminado++;
+                }
+            }
+            System.out.println();
+            Thread.sleep(1000);
+        }
+
+
+
+    }
+}
