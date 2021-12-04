b46e292504cfd269d4246fd1b41e69806c1c7961
Alex <hernandez21107@iesmarenostrum.com>
2021-12-04 18:01:22 +0100

    a

--- U3Evaluable/src/com/company/Main.java
+++ U3Evaluable/src/com/company/Main.java
@@ -8,11 +8,14 @@ public class Main {
         WriterResultados writerResultados = new WriterResultados();
         Thread thread1 = new Thread(new Buscador("god", "bible.txt", 1, writerResultados));
         Thread thread2 = new Thread(new Buscador("the", "dalton1.txt", 2, writerResultados));
+        Thread thread3 = new Thread(new Buscador("all", "dalton1.txt", 3, writerResultados));
         thread1.start();
         thread2.start();
+        thread3.start();
         try {
             thread1.join();
             thread2.join();
+            thread3.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
