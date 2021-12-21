b022a90cf43e0ead6c700317e72986a3f12400a4
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2021-12-21 18:54:39 +0100

    a

--- U4/U4/src/com/ejemplo1/Cliente.java
+++ U4/U4/src/com/ejemplo1/Cliente.java
@@ -1,14 +1,39 @@
 package com.ejemplo1;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Cliente {
     public static void main(String[] args) {
+        Socket conexion = null;
         try {
-            Socket conexion = new Socket("127.0.0.1", 12345);
+            System.out.println("CLIENTE");
+            System.out.println("Iniciando conexión");
+            conexion = new Socket("127.0.0.1", 1234);
+            System.out.println("Conexión establecida");
+
+            while (true) {
+                InputStream entrada = conexion.getInputStream();
+                DataInputStream flujoEntrada = new DataInputStream(entrada);
+
+                String linea = flujoEntrada.readUTF();
+                System.out.println("Texto recibido: " + linea);
+
+                System.out.print("Escribe algo que enviar al servidor: ");
+                DataOutputStream flujoSalida = new DataOutputStream(conexion.getOutputStream());
+                flujoSalida.writeUTF(new Scanner(System.in).nextLine());
+                System.out.println("Enviando información al servidor");
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                conexion.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
--- U4/U4/src/com/ejemplo1/Servidor.java
+++ U4/U4/src/com/ejemplo1/Servidor.java
@@ -1,20 +1,42 @@
 package com.ejemplo1;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Servidor {
     public static void main(String[] args) {
+        Socket cliente = null;
+        ServerSocket servidor = null;
         try {
             System.out.println("SERVIDOR");
-            ServerSocket servidor = new ServerSocket(1234);
+            servidor = new ServerSocket(1234);
             System.out.println("Esperando cliente");
-            Socket cliente = servidor.accept();
+            cliente = servidor.accept();
             System.out.println("Cliente conectado");
-            servidor.close();
+
+            while (true) {
+                OutputStream salida = cliente.getOutputStream();
+                DataOutputStream flujoSalida = new DataOutputStream(salida);
+                System.out.print("Introduce el texto que quieres enviar: ");
+                flujoSalida.writeUTF(new Scanner(System.in).nextLine());
+                System.out.println("Enviando información al cliente");
+                DataInputStream flujoEntrada = new DataInputStream(cliente.getInputStream());
+                System.out.println("Texto recibido: " + flujoEntrada.readUTF());
+            }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                cliente.close();
+                servidor.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
