b3573c7caba2c5b9b01b5f5cd30b627e76e8409e
José Hernández <71669454+jose-hr14@users.noreply.github.com>
2022-02-26 13:43:15 +0100

    Update App.java

--- U5Evaluable/src/main/java/org/example/App.java
+++ U5Evaluable/src/main/java/org/example/App.java
@@ -18,7 +18,7 @@ public class App
         try {
             Document doc = Jsoup.connect("https://www.elcorteingles.es/ordenadores-baratos/electronica/").get();
             Elements links = doc.getElementsByClass("js_product_preview product_preview   _on_sale js_has_bazaar_voice  c12");
-            for (Element link: links) {
+             for (Element link: links) {
                 System.out.print(link.getElementsByClass("product_preview-desc").text() + " ");
                 System.out.println(link.getElementsByClass("price _big _sale").text());
             }
